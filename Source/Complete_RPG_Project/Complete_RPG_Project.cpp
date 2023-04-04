165bc3e7eaf3d93b5bb9c0ccf02dd605717d7b61
safa <63610742+thekrocker@users.noreply.github.com>
2023-04-05 00:18:28 +0300

    part 2 commit

---
+++ Source/Complete_RPG_Project/Complete_RPG_Project.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Complete_RPG_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Complete_RPG_Project, "Complete_RPG_Project" );
